695a51deeaeb7321395e91fe41c81397eee0d410
qian_js <icemanchester@163.com>
2017-07-16 01:36:03 +0800

    增加瀑布布局组件，自适应布局组件，更新测试网页

--- project/css/jslib.js
+++ project/css/jslib.js
@@ -11,6 +11,39 @@ $(document).ready(function() {
 /*--------汉堡包菜单组件over----------------------------*/
 
 
+/*--------瀑布布局start------------------*/
+//定义瀑布布局函数，外部传入参数： rowItemNum 一行的个数
+jQuery.fn.waterFlowSet=function(rowItemNum){
+    var oneRowItemNum = rowItemNum;
+    var heightArr = [];
+    var waterflowContainerWidth = $(".waterflowContainer").width();
+    console.log("container的宽度"+ waterflowContainerWidth);
+    console.log("瀑布布局里有的item数量:"+$(".waterflowContainer .waterflowItem").length);
+    $(".waterflowContainer .waterflowItem").each(function(index, el) {
+        if(undefined  == heightArr[index%oneRowItemNum]){
+            $(this).css('top', '0px');
+            console.log("第:"+index+"个元素top:0px");
+        }else{
+            $(this).css('top', heightArr[index%oneRowItemNum]);
+            console.log("第:"+index+"个元素top:"+heightArr[index%oneRowItemNum]+"px");
+        }
+        $(this).css('width', waterflowContainerWidth/oneRowItemNum);
+        $(this).css('left', (index%oneRowItemNum)*waterflowContainerWidth/oneRowItemNum);
+        heightArr[index%oneRowItemNum]=$(this).height();
+        console.log("第:"+index+"个元素的高度是"+$(this).height());
+    });
+}
+//在window的load事件里面初始化布局，并且托管到
+$(".waterflowContainer").waterFlowSet(3); //对外部组件使用瀑布布局，传入参数为4代表一行4个元素，元素不一定要为图片的
+$(window).resize(function() {
+    console.log("触发了window resize事件");
+    $(".waterFlowSet").waterFlowSet(3);
+});
+
+
+/*--------瀑布布局end------------------*/
+
+
 
 
 
